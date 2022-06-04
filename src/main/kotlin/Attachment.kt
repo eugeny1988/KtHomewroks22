ccc124679e9eef4d0b48ca9541165e68bd5c10d8
Евгений <eugeny1988@gmail.com>
2022-06-05 02:14:01 +0300

    Added new classes

--- src/main/kotlin/Attachment.kt
+++ src/main/kotlin/Attachment.kt
@@ -1,3 +1,4 @@
+
 interface Attachment {
     abstract val type: String
 }
@@ -11,17 +12,52 @@ data class Video(
     val firstFrame: Array<firstFrame>,
 
 
-) : Attachment {
+    ) : Attachment {
     override val type = "video"
-
-
 }
+
+data class Photo(
+    val id: Int,
+    val albumId: Int,
+    val ownerId: Int,
+    val userId: Int,
+    val text: String,
+    val date: Int
+
+) : Attachment {
+    override val type = "photo"
+}
+
+data class Audio(
+    val id: Int,
+    val ownerId: Int,
+    val artist: String,
+    val title: String,
+    val duration: Int
+) : Attachment {
+    override val type = "audio"
+}
+
+data class File(
+    val id: Int,
+    val ownerId: Int,
+    val title: String,
+    val size: Int,
+    val ext: String,
+    val url: String,
+    val date: Int,
+    val typeFile: Int
+) : Attachment {
+    override val type = "file"
+}
+
 data class Image(
     val height: Int,
     val url: String,
     val width: Int,
     val withPadding: Int
 )
+
 data class firstFrame(
     val height: Int,
     val url: String,
